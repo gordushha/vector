9c907d39807b6d6271c6c42a33f1863dab023948
gordushha <gordey.mary17@gmail.com>
2020-05-19 19:02:04 +0300

    upd

--- ConsoleApplication24/Matrix.h
+++ ConsoleApplication24/Matrix.h
@@ -8,281 +8,38 @@ template<typename T>
 class Matrix
 {
 public:
-  Matrix():M(0), n(0),m(0){}
+	Matrix() :M(0), n(0), m(0) {}
 
-  Matrix(const Vector<T>& v1);
-  Matrix(size_t size1, size_t size2, T** M_);
-  
-  ~Matrix();
-  
-  Matrix operator +(const Matrix& other);
-  Matrix operator *(const Matrix& other);
-  
-  Matrix(int size1, int size2);
-  
-  Matrix(const Matrix& other);
-  
+	Matrix(const Vector<T>& v1);
+	Matrix(size_t size1, size_t size2, T** M_);
 
-  Matrix operator -(const Matrix& other);
- 
+	~Matrix();
 
-  Matrix& operator=(const Matrix& other);
-  
-  bool operator==(const Matrix<T>& other);
-  
-  const T* operator [](size_t i) const;
- 
-  T* operator [](size_t i);
-  int GetN() const;
-  int GetM() const;
-  
+	Matrix operator +(const Matrix& other);
+	Matrix operator *(const Matrix& other);
+
+	Matrix(int size1, int size2);
+
+	Matrix(const Matrix& other);
+
+
+	Matrix operator -(const Matrix& other);
+
+
+	Matrix& operator=(const Matrix& other);
+
+	bool operator==(const Matrix<T>& other);
+
+	const T* operator [](size_t i) const;
+
+	T* operator [](size_t i);
+	int GetN() const;
+	int GetM() const;
+
 private:
-  T** M;
-  size_t n;
-  size_t m;
+	T** M;
+	size_t n;
+	size_t m;
 };
 
-template <typename T>
-Matrix<T>::Matrix(const Vector<T>& v1)
-{
-  n = v1.GetN();
-  m = 1;
-
-  M = new T * [n];
-  for (int i = 0; i < n; i++)
-    M[i] = new T[m];
-  for (int i = 0; i < n; i++)
-    
-      M[i][0] = v1[i];
-
-}
-template <typename T>
-  Matrix<T>::Matrix(size_t size1, size_t size2, T** M_)
-  {
-    n = size1;
-    m = size2;
-
-    M = new T * [n];
-    for (int i = 0; i < n; i++)
-      M[i] = new T[m];
-    for (int i = 0; i < n; i++)
-      for (int j = 0; j < m; j++)
-        M[i][j] = M_[i][j];
-  }
-
-  template <typename T>
-  Matrix<T>::~Matrix()
-  {
-
- 
- 
-      for (int i = 0; i < n; i++)
-        delete[] M[i];
-      delete[] M;
-     
-      n = 0;
-      m = 0;
-
-  }
-
-  template <typename T>
-  Matrix<T> Matrix<T>::operator +(const Matrix& other)
-  {
-    if ((this->m != other.m) || (this->n != other.n))
-      throw length_error("-1");
-    for (int k = 0; k < n; k++) {
-      for (int i = 0; i < m; i++) {
-        (*this).M[k][i] = (*this).M[k][i] + other.M[k][i];
-      }
-    }
-    return *this;
-  }
-
-  template <typename T>
-  Matrix<T>::Matrix(int size1, int size2)
-  {
-    n = size1;
-    m = size2;
-    /*
-    M = new T * [n];
-    for (int i = 0; i < n; i++)
-      M[i] = new T[m];
-    for (int i = 0; i < n; i++)
-      for (int j = 0; j < m; j++)
-        M[i][j] = rand() % 100;
-        */
-    M = new T*[size1];
-    for (int i = 0; i < size1; i++)
-    {
-        M[i] = new T[size2];
-        for (int j = 0; j < size2; j++)
-            M[i][j] = 0;
-    }
-  }
-
-  template <typename T>
-  Matrix<T>::Matrix(const Matrix& other)
-  {
-    n = other.n;
-    m = other.m;
-    M = new T * [n];
-
-    for (int i = 0; i < n; i++)
-      M[i] = new T[m];
-
-    for (int i = 0; i < n; i++)
-      for (int j = 0; j < m; j++)
-        M[i][j] = other.M[i][j];
-  }
-
-  template <typename T>
-   Matrix<T> Matrix<T>::operator -(const Matrix& other)
-  {
-    Matrix newmatrix;
-    for (int k = 0; k < n; k++) {
-      for (int i = 0; i < m; i++) {
-        newmatrix[k][i] = M[k][i] - other.M[k][i];
-      }
-    }
-    return newmatrix;
-  }
-
-  template <typename T>
-  Matrix<T> operator * (const Matrix<T>& x, const Vector<T>& y)
-  {
-    return x * Matrix<T>(y);
-
-  }
-
-  template <typename T>
-  Matrix<T> operator * (const Vector<T>& x, const Matrix<T>& y)
-  {
-    return Matrix<T>(x) * y;
-
-  }
-  
-  template <typename T>
-  int Matrix<T>::GetM() const
-  {
-    return m;
-  }
-
-  template <typename T>
-  int Matrix<T>::GetN() const
-  {
-    return n;
-  }
-
-  template <typename T>
-  Matrix<T>& Matrix<T>::operator=(const Matrix& other)
-  {
-    if (this == &other)
-      return *this;
-    for (int i = 0; i < n; i++)
-      delete[] M[i];
-    delete[] M;
-    this->n = other.n;
-    this->m = other.m;
-    M = new T * [n];
-    for (int i = 0; i < n; i++)
-      M[i] = new T[m];
-    for (int i = 0; i < n; i++)
-      for (int j = 0; j < m; j++)
-        M[i][j] = other.M[i][j];
-    return *this;
-  }
-
-  template <typename T>
-  bool Matrix<T>::operator==(const Matrix<T>& other)
-  {
-    if (n == other.n && m == other.m)
-    {
-
-      for (int i = 0; i < n; i++)
-        for (int j = 0; j < m; j++)
-          if (M[i][j] != other.M[i][j])
-            return false;
-      return true;
-    }
-    else
-      return false;
-  }
-
-  template <typename T>
- const  T* Matrix<T>::operator [](size_t i) const
-  {
-    if (i >= n)
-      throw  std::runtime_error("wrong index");
-
-    else return M[i];
-
-  }
-
- template <typename T>
-  T* Matrix<T>::operator [](size_t i)
- {
-   if (i >= n)
-     throw  std::runtime_error("wrong index");
-
-   else return M[i];
-
- }
- template <typename T>
- std::ostream& operator<<(std::ostream& stream, const Matrix<T>& self)
- {
-
-   for (size_t index = 0; index < self.GetN(); index++)
-     for (size_t k = 0; k < self.GetM();k++)
-     stream << self[index][k] << std::endl;
-   return stream;
- }
-
- template <typename T>
- std::istream& operator>>(std::istream& stream,  Matrix<T>& self)
- {
-
-   size_t size1 = 0;
-   size_t size2 = 0;
-
-   stream >> size1;
-   stream >> size2;
-   Matrix <T> v4(size1,size2);
-   for (size_t index = 0; index < size1; index++)
-     for (size_t k = 0; k < size2; k++)
-     stream >> v4[index][k];
-   self = v4;
-   return stream;
- }
-
-  
-  template <typename T>
-  Matrix<T> Matrix<T>::operator*(const Matrix<T>& other)
-  {
-    auto start = high_resolution_clock::now();
-    if (this->m == other.n)
-    {
-      Matrix<T> result;
-      result.n = this->n;
-      result.m = other.m;
-      result.M = new T * [result.n];
-      for (int i = 0; i < result.n; i++)
-        result.M[i] = new T[result.m];
-
-      for (int i = 0; i < result.n; i++)
-        for (int j = 0; j < result.m; j++)
-        {
-          result.M[i][j] = 0;
-          for (int k = 0; k < this->m; k++)
-          {
-            result.M[i][j] = result.M[i][j] + (M[i][k] * other.M[k][j]);
-          }
-        }
-      return result;
-    }
-    else
-      throw length_error("-1");
-    auto stop = high_resolution_clock::now();
-    auto duration = duration_cast<microseconds>(stop - start);
-    cout << "Time taken by multiplication: "
-      << duration.count() << " microseconds" << endl;
-  }
+#include "Matrix_i.h"
--- ConsoleApplication24/main.cpp
+++ ConsoleApplication24/main.cpp
@@ -4,75 +4,75 @@
 using namespace std;
 
 
-
 int main()
 
 {
-  srand(time(NULL));
-    /*Vector<double> v1(5);
-    v1[2] = 42;
-    std::cout << v1 << std::endl;
-    Vector<double> v2(v1);
-    std::cout << v2 << std::endl;
-    Vector <double> v3;
-     v1 + v2;
-    cout << v1 << endl << " + " << endl << v2 << endl << " = " << endl << v3 << endl;
-    v3 = v1 - v2;
-    cout << v1 << endl << " - " << endl << v2 << endl << " = " << endl << v3 << endl;
-    v3 = v1 * v2;
-    cout << v1 << endl << "*" << endl << v2 << endl << " = " << endl << v3 << endl;
-    
-    v3.quicksort();
+	srand(time(NULL));
+	Vector<double> v1(5);
+	v1[2] = 42;
+	Vector<double> v2(v1);
+	Vector <double> v3;
+	v1 + v2;
 
 
-    Matrix <double> m1(2, 2);
-    Matrix<double> m2(2, 2);
-    Matrix<double> M;
-    M = m1 * m2;
-    cout << m1 << endl << " * " << endl << m2 << endl << " = " << endl << M << endl;
-    M = m1 + m2;
-    cout << m1 << endl << " + " << endl << m2 << endl << " = " << endl << M << endl;
-    M = m1 * m2;
-    cout << m1 << endl << " * " << endl << m2 << endl << " = " << endl << M << endl;*/
+	Matrix <double> m1(2, 2);
+	Matrix<double> m2(2, 2);
+	Matrix<double> M;
+	M = m1 + m2;
 
-    int t = 10;
+	int t = 10;
 
-    for (int i = 1; i < 5; i++)
-    {
-        Vector<double> v1(t);
-        v1.bubblesort();
-        t = t * 10;
+	for (int i = 1; i < 5; i++)
+	{
+		cout << "t = " << t << endl;
+		Vector<double> v1(t);
+		v1.bubblesort();
+		t = t * 10;
 	}
 
-    t = 10;
-            cout << endl;
+	t = 10;
+	cout << endl;
 
-    for (int i = 1; i < 5; i++)
-    {
-        Vector<double> v1(t);
-        v1.insertionsort();
-        t = t * 10;
+	for (int i = 1; i < 5; i++)
+	{
+		cout << "t = " << t << endl;
+		Vector<double> v1(t);
+		v1.insertionsort();
+		t = t * 10;
 	}
 
-    t = 10;
-            cout << endl;
+	t = 10;
+	cout << endl;
 
-    for (int i = 1; i < 5; i++)
-    {
-        Vector<double> v1(t);
-        v1.quicksort();
-        t = t * 10;
+	for (int i = 1; i < 5; i++)
+	{
+		cout << "t = " << t << endl;
+		Vector<double> v1(t);
+		v1.quicksort();
+		t = t * 10;
 	}
 
-    for (int i = 1; i < 5; i++)
-    {
-      Matrix<int> m1(t,t);
-      m1*m1;
-      t = t * 10;
-    }
+	t = 10;
+	cout << endl;
 
-    return 0;
+	for (int i = 1; i < 4; i++)
+	{
+		cout << "t = " << t << endl;
+		Matrix<int> m1(t, t);
+		m1 + m1;
+		t = t * 10;
+	}
 
- 
-  //catch (length_error& ab) { cout << ab.what() << endl;
+	t = 1;
+	cout << endl;
+
+	for (int i = 1; i < 4; i++)
+	{
+		cout << "t = " << t << endl;
+		Matrix<int> m1(t, t);
+		m1* m1;
+		t = t * 10;
+	}
+
+	return 0;
 }
